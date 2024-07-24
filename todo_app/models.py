509e308965c10d7e235105bf953f20b2a08f7801
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-07-24 10:28:40 +0300

    add models and migrations for them

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline_time = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag")
+
+    def __str__(self):
+        return f"{self.content}, created at - {self.created_at}, tags - {self.tags.name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
