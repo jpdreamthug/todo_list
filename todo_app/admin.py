80628a88a40e7c26105752f351f1aa04ee4c9dfa
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-07-24 10:29:13 +0300

    add admin page with many-to-many field

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_app.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "created_at", "deadline_time", "is_completed", "get_tags"]
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related("tags")
+
+    def get_tags(self, obj):
+        return ", ".join([task.name for task in obj.tags.all()])
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    ...
